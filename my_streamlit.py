608a5b9d16472232e37f97682455ad540b2a8b7e
Rayhan Ozzy Ertarto <73189702+rayhanozzy@users.noreply.github.com>
2022-01-14 22:35:50 +0700

    Update my_streamlit.py

--- my_streamlit.py
+++ my_streamlit.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import matplotlib.pyplot as plt
-#import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 option = st.sidebar.selectbox(
     'Silakan pilih:',
