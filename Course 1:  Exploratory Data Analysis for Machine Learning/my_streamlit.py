5ca3c978d8b2097fc5c6dc74580e72585a28ff5f
Rayhan Ozzy Ertarto <73189702+rayhanozzy@users.noreply.github.com>
2022-01-14 19:04:38 +0700

    Add files via upload

---
+++ Course 1:  Exploratory Data Analysis for Machine Learning/my_streamlit.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+option = st.sidebar.selectbox(
+    'Silakan pilih:',
+    ('Home','Dataframe','Chart')
+)
+
+if option == 'Home' or option == '':
+    st.write("""# Halaman Utama""")
+elif option == 'Dataframe':
+    st.write("""## Dataframe""")
+
+    data = pd.read_csv(
+        "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-03-12/board_games.csv")
+    data.head(5)
+elif option == 'Chart':
+    st.write("""## Draw Charts""")
+
+    chart_data = pd.DataFrame(
+        np.random.randn(30,2),
+        columns = ['a','b']
+    )
+
+    st.line_chart(chart_data)
+
+    chart_data
