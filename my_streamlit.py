0612944651cd56be1756fb5bb2cd2b847719656f
Rayhan Ozzy Ertarto <73189702+rayhanozzy@users.noreply.github.com>
2022-01-15 12:34:58 +0700

    Update my_streamlit.py

--- my_streamlit.py
+++ my_streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
-#import seaborn as sns
+import seaborn as sns
 
 option = st.sidebar.selectbox(
     'Menu:',
