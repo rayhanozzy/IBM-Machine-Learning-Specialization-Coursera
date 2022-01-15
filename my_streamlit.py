fe235f2795ba980900383a3d6f8ba63f53e0b338
Rayhan Ozzy Ertarto <73189702+rayhanozzy@users.noreply.github.com>
2022-01-15 12:57:27 +0700

    Update my_streamlit.py

--- my_streamlit.py
+++ my_streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 #import seaborn as sns
 
 option = st.sidebar.selectbox(
